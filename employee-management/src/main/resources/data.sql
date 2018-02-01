c20eff0db11f8c255ab44ec91f8090c5bacd71e6
shiva vadloori <shiva@gspann.com>
2018-02-01 15:06:52 +0530

    Insert statements have been added for Employee table

---
+++ employee-management/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO User (id, username, password) VALUES (1, 'shiva', '$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu');
+INSERT INTO User (id, username, password) VALUES (2, 'balram', '$2a$04$PCIX2hYrve38M7eOcqAbCO9UqjYg7gfFNpKsinAxh99nms9e.8HwK');
+INSERT INTO User (id, username, password) VALUES (3, 'varun', '$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu');
+
+
+
+
+INSERT INTO Employee(designation,employee_name)  VALUES('SSE','varu');
+INSERT INTO Employee(designation,employee_name)  VALUES('SE','shvia');
+INSERT INTO Employee(designation,employee_name)  VALUES('SSE','balaram');
+INSERT INTO Employee(designation,employee_name)  VALUES('SE','shiva');
